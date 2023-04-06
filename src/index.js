bfac8d2a4f173bd09247ab692cbc330d3ce9a515
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2023-04-05 23:40:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const UnloadPrompt = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleUnload = (event) => {
-      // Cancel the event as stated by the standard.
-      event.preventDefault();
-
-      // Chrome requires returnValue to be set.
-      event.returnValue = '';
-
-      // Save the current location in local storage.
-      localStorage.setItem('unloadLocation', navigate.location.pathname);
-    };
-
-    window.addEventListener('beforeunload', handleUnload);
-
-    // Remove the event listener on cleanup.
-    return () => {
-      window.removeEventListener('beforeunload', handleUnload);
-    };
-  }, [navigate]);
-
-  return <App />;
-};
-
-export default UnloadPrompt;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
