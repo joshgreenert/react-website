c829177ccbff2822098fb501c0d1fb831ed3aaa9
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2023-04-05 23:21:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import App from './App';
 
 const UnloadPrompt = () => {
