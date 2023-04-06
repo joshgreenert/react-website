22ff1b24f6de6b87f18c3def25ff9658d91e6d4d
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2023-04-05 23:18:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const UnloadPrompt = () => {
+  const history = useHistory();
 
+  useEffect(() => {
+    const handleUnload = (event) => {
+      // Cancel the event as stated by the standard.
+      event.preventDefault();
 
+      // Chrome requires returnValue to be set.
+      event.returnValue = '';
 
+      // Save the current location in local storage.
+      localStorage.setItem('unloadLocation', history.location.pathname);
+    };
 
+    window.addEventListener('beforeunload', handleUnload);
+
+    // Remove the event listener on cleanup.
+    return () => {
+      window.removeEventListener('beforeunload', handleUnload);
+    };
+  }, [history]);
+
+  return <App />;
+};
+
+export default UnloadPrompt;
