b74e774369d4eda6a51b57163c7d24a0366c37a3
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2023-04-06 20:13:33 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import { Button } from "./Button";
 import './Navbar.css';
 
@@ -27,37 +27,37 @@ function Navbar(){
         <div>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                    <NavLink  to="/" className="navbar-logo" exact onClick={closeMobileMenu}>
                         LOGO
-                    </Link>
+                    </NavLink >
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
-                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                            <NavLink to='/' className="nav-links" onClick={closeMobileMenu}>
                                 Home
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/github/' className="nav-links" onClick={closeMobileMenu}>
+                            <NavLink to='/github/' className="nav-links" onClick={closeMobileMenu}>
                                 GitHub
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/resume/' className="nav-links" onClick={closeMobileMenu}>
+                            <NavLink to='/resume/' className="nav-links" onClick={closeMobileMenu}>
                                 Resume
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/resources/' className="nav-links" onClick={closeMobileMenu}>
+                            <NavLink to='/resources/' className="nav-links" onClick={closeMobileMenu}>
                                 Resources
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/contact/' className="nav-links" onClick={closeMobileMenu}>
+                            <NavLink to='/contact/' className="nav-links" onClick={closeMobileMenu}>
                                 Contact
-                            </Link>
+                            </NavLink>
                         </li>
                     </ul>
                     {button && <Button buttonStyle='btn--outline' location="/react-website/contact">Contact</Button>}
